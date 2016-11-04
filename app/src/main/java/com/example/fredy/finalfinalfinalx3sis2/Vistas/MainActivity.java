52ed8beb8218929f782b9659071c21f2275f6941
Fredy Hernandez <fredy.hernandez@pucp.pe>
2016-11-04 14:40:50 -0500

    solve problems of R.

--- app/src/main/java/com/example/fredy/finalfinalfinalx3sis2/Vistas/MainActivity.java
+++ app/src/main/java/com/example/fredy/finalfinalfinalx3sis2/Vistas/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.fredy.finalfinalfinalx3sis2.Vistas;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.fredy.finalfinalfinalx3sis2.Controller.GestorCliente;
 import com.example.fredy.finalfinalfinalx3sis2.R;
@@ -13,10 +16,12 @@ public class MainActivity extends AppCompatActivity {
     private GestorCliente GestorCliente;
     private EditText loginUsuario;
     private EditText loginContrasenia;
+    final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button but1 = (Button) findViewById(R.id.buttonIngresar);
         loginUsuario = (EditText) findViewById(R.id.loginUsuario);
         loginContrasenia = (EditText) findViewById(R.id.loginContrasenia);
@@ -25,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (verificarUsuario()){
+                    Intent perfil = new Intent(context, Perfil.class);
+                    startActivity(perfil);
+                }else{
+                    Context context = getApplicationContext();
+                    CharSequence text = "error de usuario o contraseña";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
 
                 }
             }
---
+++ app/src/main/java/com/example/fredy/finalfinalfinalx3sis2/Vistas/Perfil.java
@@ -0,0 +1,8 @@
+package com.example.fredy.finalfinalfinalx3sis2.Vistas;
+
+/**
+ * Created by fredy on 11/4/16.
+ */
+
+public class Perfil {
+}
