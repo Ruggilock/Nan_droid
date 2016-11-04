e1e5233025df8341b633ea1fb8b029da24f14e90
Fredy Hernandez <fredy.hernandez@pucp.pe>
2016-11-04 18:29:25 -0500

    18:034 y aun no corre

---
+++ app/src/main/java/com/example/fredy/finalfinalfinalx3sis2/Vistas/HacerPedido.java
@@ -0,0 +1,18 @@
+package com.example.fredy.finalfinalfinalx3sis2.Vistas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.fredy.finalfinalfinalx3sis2.R;
+
+/**
+ * Created by fredy on 11/4/16.
+ */
+
+public class HacerPedido extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hacer_pedido);
+    }
+}
--- app/src/main/java/com/example/fredy/finalfinalfinalx3sis2/Vistas/Hacer_pedido_catalogo.java
+++
@@ -1,8 +0,0 @@
-package com.example.fredy.finalfinalfinalx3sis2.Vistas;
-
-/**
- * Created by fredy on 11/4/16.
- */
-
-public class Hacer_pedido_catalogo {
-}
